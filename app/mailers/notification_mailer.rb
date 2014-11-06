d6d213e64fab87eac1354eb47f7da92446668baa
Scott Albertson <hello@scottalbertson.com>
2014-11-06 08:14:31 -0800

    Inline #service_title

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,17 +2,11 @@ class NotificationMailer < ActionMailer::Base
   default from: "billybob@example.com"
 
   def notification(service)
-    @service_name = service_name(service)
+    @service_name = service.to_s.titleize
 
     mail(
       to: "hello@scottalbertson.com",
       subject: "#{@service_name} went down!"
     )
-  end
-
-  private
-
-  def service_title(service)
-    service.to_s.titleize
   end
 end
