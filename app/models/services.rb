661d7a72b778d0ef5eebb04764cf5cf43a4d7d60
Scott Albertson <hello@scottalbertson.com>
2014-11-11 22:03:36 -0800

    Constantize string differently
    * For some reason "travis" couldn't be constantized

--- app/models/services.rb
+++ app/models/services.rb
@@ -1,11 +1,11 @@
 class Services
   def self.all
     Dir.glob("app/services/*").map do |filepath|
-      File.basename(filepath, ".rb").classify.constantize.new
+      File.basename(filepath, ".rb").titleize.gsub(/\s+/, "").constantize.new
     end
   end
 
   def self.find(id)
-    id.to_s.classify.constantize.new
+    id.to_s.titleize.gsub(/\s+/, "").constantize.new
   end
 end
