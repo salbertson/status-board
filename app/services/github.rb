1a1246370289674432775fa236573ed9d829f003
Scott Albertson <hello@scottalbertson.com>
2014-11-13 09:52:45 -0800

    Use GitHub status API instead of status page

--- app/services/github.rb
+++ app/services/github.rb
@@ -6,11 +6,11 @@ class Github < Service
   end
 
   def url
-    'https://status.github.com'
+    'https://status.github.com/api/status.json'
   end
 
   def down?
-    document = Nokogiri::HTML(open(url))
-    document.css('#message').text.include?("All systems operational") == false
+    response = Typhoeus.get(url)
+    JSON.parse(response.body)["status"] != "good"
   end
 end
