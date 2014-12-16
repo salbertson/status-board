a9654dd85b3883fde49711db80b01c994f6efe52
Neal Kemp <me@nealke.mp>
2014-12-16 14:34:06 -0800

    Fetch github client

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :github,
-    ENV['GITHUB_CLIENT_ID'],
-    ENV['GITHUB_CLIENT_SECRET'],
+    ENV.fetch('GITHUB_CLIENT_ID'),
+    ENV.fetch('GITHUB_CLIENT_SECRET'),
     scope: 'user:email'
   )
 end
