0c3347d3ad0a1cf3958ed4088f15cc81c07dd10b
Scott Albertson <hello@scottalbertson.com>
2014-12-22 09:01:28 -0800

    Promote Status Board in status message

--- app/integrations/slack.rb
+++ app/integrations/slack.rb
@@ -6,7 +6,7 @@ class Slack
   def notify(service)
     Typhoeus.post(
       @settings["url"],
-      body: "#{service.name} might be having issues - #{service.url}"
+      body: "#{service.name} might be having issues - #{service.url} (via #{root_url})"
     )
   end
 end
