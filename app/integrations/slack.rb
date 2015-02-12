5c9655df2e79423d6c58864d8596007748618366
Scott Albertson <hello@scottalbertson.com>
2015-02-12 11:24:21 -0800

    Use environment variable for root URL

--- app/integrations/slack.rb
+++ app/integrations/slack.rb
@@ -6,7 +6,7 @@ class Slack
   def notify(service)
     Typhoeus.post(
       @settings["url"],
-      body: "#{service.name} might be having issues - #{service.url} (via #{root_url})"
+      body: "#{service.name} might be having issues - #{service.url} (via #{ENV["ROOT_URL"] || "Status Board"})"
     )
   end
 end
